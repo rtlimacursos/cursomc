928596670f60fe05bc4da9ed83166a75927dc847
Rtlima Cursos <rtlima.cursos@gmail.com>
2018-08-16 17:48:41 -0300

    item Pedido e ItemPedidoPK

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,6 +35,8 @@ public class Pedido implements Serializable {
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco endereDeEntrega;
 	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>(); 
 	public Pedido() {
 		
 	}
@@ -84,6 +89,14 @@ public class Pedido implements Serializable {
 		this.endereDeEntrega = endereDeEntrega;
 	}
 
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -108,6 +121,7 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
 	
 
 }
